b56d8cba36dd09a46b8ee968eeaa1bc89eb6fad4
Jamie Curran <jay.curran@gmail.com>
2014-01-20 17:29:05 +0000

    Initial commit.

---
+++ src/instrumentTest/java/com/threewisemonkeysdevelopment/android/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.threewisemonkeysdevelopment.android;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.threewisemonkeysdevelopment.android.MainActivityTest \
+ * com.threewisemonkeysdevelopment.android.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("com.threewisemonkeysdevelopment.android", MainActivity.class);
+    }
+
+}
